5eea59222173842163744f7b01f6e25a96597ce4
jzanzig <jmlzanzig@gmail.com>
2015-10-09 15:43:47 -0500

    update br load script

--- import/rcra/load_BR_csv.sh
+++ import/rcra/load_BR_csv.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -xv
 
-psql -f drop_table_BR.sql
-psql -f create_table_BR.sql
+RCRA_DIR=$1
+
+psql -f import/rcra/create_table_BR.sql
 
 echo loading br_reporting into the database ...
-in2csv -e iso-8859-1 -f fixed -s br_reporting_schema.csv /mnt/data/epa/RCRA_flat_files/br_reporting.txt | psql -c '\COPY br_reporting FROM STDIN WITH CSV HEADER'
+in2csv -e iso-8859-1 -f fixed -s $RCRA_DIR/br_reporting_schema.csv $RCRA_DIR/br_reporting.txt | psql -c '\COPY rcra.br_reporting FROM STDIN WITH CSV HEADER'
 echo br_reporting has been loaded into the database.
 
 echo loading bgm_waste_code into the database ...
-in2csv -e iso-8859-1 -f fixed -s bgm_waste_code_schema.csv /mnt/data/epa/RCRA_flat_files/bgm_waste_code.txt | psql -c '\COPY bgm_waste_code FROM STDIN WITH CSV HEADER'
+in2csv -e iso-8859-1 -f fixed -s $RCRA_DIR/bgm_waste_code_schema.csv $RCRA_DIR/bgm_waste_code.txt | psql -c '\COPY rcra.bgm_waste_code FROM STDIN WITH CSV HEADER'
 echo bgm_waste_code has been loaded into the database.
 
 echo loading bwr_waste_code into the database ...
-in2csv -e iso-8859-1 -f fixed -s bwr_waste_code_schema.csv /mnt/data/epa/RCRA_flat_files/bwr_waste_code.txt | psql -c '\COPY bwr_waste_code FROM STDIN WITH CSV HEADER'
+in2csv -e iso-8859-1 -f fixed -s $RCRA_DIR/bwr_waste_code_schema.csv $RCRA_DIR/bwr_waste_code.txt | psql -c '\COPY rcra.bwr_waste_code FROM STDIN WITH CSV HEADER'
 echo bwr_waste_code has been loaded into the database.
 
