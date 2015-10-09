8bdca15cc12d4031934020612479c2f388412975
jzanzig <jmlzanzig@gmail.com>
2015-10-09 15:45:44 -0500

    Update load_F_csv.sh

--- import/rcra/load_F_csv.sh
+++ import/rcra/load_F_csv.sh
@@ -1,21 +1,22 @@
 #!/bin/bash -xv
 
-psql -f drop_table_F.sql
-psql -f create_table_F.sql
+RCRA_DIR=$1
+
+psql -f import/rcra/create_table_F.sql
 
 echo loading fcost_estimate into the database ...
-in2csv -e iso-8859-1 -f fixed -s fcost_estimate_schema.csv /mnt/data/epa/RCRA_flat_files/fcost_estimate.txt | psql -c '\COPY fcost_estimate FROM STDIN WITH CSV HEADER'
+in2csv -e iso-8859-1 -f fixed -s $RCRA_DIR/fcost_estimate_schema.csv $RCRA_DIR/fcost_estimate.txt | psql -c '\COPY rcra.fcost_estimate FROM STDIN WITH CSV HEADER'
 echo fcost_estimate has been loaded into the database.
 
 echo loading fln_cost_mechanism_detail into the database ...
-in2csv -e iso-8859-1 -f fixed -s fln_cost_mechanism_detail_schema.csv /mnt/data/epa/RCRA_flat_files/fln_cost_mechanism_detail.txt | psql -c '\COPY fln_cost_mechanism_detail FROM STDIN WITH CSV HEADER'
+in2csv -e iso-8859-1 -f fixed -s $RCRA_DIR/fln_cost_mechanism_detail_schema.csv $RCRA_DIR/fln_cost_mechanism_detail.txt | psql -c '\COPY rcra.fln_cost_mechanism_detail FROM STDIN WITH CSV HEADER'
 echo fln_cost_mechanism_detail has been loaded into the database.
 
 echo loading fmechanism_detail into the database ...
-in2csv -e iso-8859-1 -f fixed -s fmechanism_detail_schema.csv /mnt/data/epa/RCRA_flat_files/fmechanism_detail.txt | psql -c '\COPY fmechanism_detail FROM STDIN WITH CSV HEADER'
+in2csv -e iso-8859-1 -f fixed -s $RCRA_DIR/fmechanism_detail_schema.csv $RCRA_DIR/fmechanism_detail.txt | psql -c '\COPY rcra.fmechanism_detail FROM STDIN WITH CSV HEADER'
 echo fmechanism_detail has been loaded into the database.
 
 echo loading fmechanism into the database ...
-in2csv -e iso-8859-1 -f fixed -s fmechanism_schema.csv /mnt/data/epa/RCRA_flat_files/fmechanism.txt | psql -c '\COPY fmechanism FROM STDIN WITH CSV HEADER'
+in2csv -e iso-8859-1 -f fixed -s $RCRA_DIR/fmechanism_schema.csv $RCRA_DIR/fmechanism.txt | psql -c '\COPY rcra.fmechanism FROM STDIN WITH CSV HEADER'
 echo fmechanism has been loaded into the database.
 
