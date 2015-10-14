4a1d884a2f6412e4217b4a51bd69b9e69b2a6b7a
jzanzig <jzanzig@ip-10-24-1-5.us-west-2.compute.internal>
2015-10-14 15:39:18 +0000

    remove import script add load script

--- import/tri/load_tri_data.sh
+++ import/tri/load_tri_data.sh
@@ -7,20 +7,6 @@ TRI_DIR=$1
 psql -f import/tri/create_schema_tri.sql
 psql -f import/tri/create_table_tri.sql
 
-# Create all the tables and Load the data
-
-for i in 1 "2a" "2b" "3a" "3b" 4 5 6; 
-do
-    head -1 $TRI_DIR/US_${i}_1987_v13.txt > $TRI_DIR/tri_${i}.txt; tail -q -n +2 $TRI_DIR/US_${i}_*_v13.txt >> $TRI_DIR/tri_${i}.txt 
-    in2csv -t -f csv -e iso-8859-1 $TRI_DIR/tri_${i}.txt > $TRI_DIR/tri.type_${i}.csv 
-
-#    csvsql -i postgresql --no-constraints --table tri.${i} $TRI_DIR/tri_${i}.csv | perl -pe '$_= lc($_)' | perl -pe 's:"[^"]*":($x=$&)=~s/ /_/g;$x:ge' > import/tri/create_table_${i}.sql
-
-#    psql -f import/tri/create_table_${i}.sql 1>/dev/null 2> import/tri/create_table_${i}.log
-#    psql -c "\copy tri.type_${i} from stdin with csv header"
-done
-
-
 echo loading tri.type_1.csv into the database ...
 cat $TRI_DIR/tri.type_1.csv | psql -h dssgsummer2014postgres.c5faqozfo86k.us-west-2.rds.amazonaws.com -U epa -d epa -c '\COPY tri.type_1 FROM STDIN WITH CSV HEADER;'
 echo tri.type_1.csv has been loaded into the database.
