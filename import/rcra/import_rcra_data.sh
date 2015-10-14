e1254337abcd57ce33694de1b1f4587e8d120e7a
jzanzig <jmlzanzig@gmail.com>
2015-10-14 16:09:45 -0500

    Update import_rcra_data.sh

--- import/rcra/import_rcra_data.sh
+++ import/rcra/import_rcra_data.sh
@@ -1,17 +1,12 @@
 #!/bin/bash -xv
 
+RCRA_DIR=$1
+
 # Make the load scripts and SQL files
 psql -f import/rcra/create_schema_rcra.sql
-
-psql -f import/rcra/create_table_A.sql
-psql -f import/rcra/create_table_BR.sql
-psql -f import/rcra/create_table_C.sql
-psql -f import/rcra/create_table_F.sql
-psql -f import/rcra/create_table_gis.sql
-psql -f import/rcra/create_table_H.sql
-psql -f import/rcra/create_table_P.sql
+psql -f import/rcra/create_table_\*.sql
 
 # Loop over the load scripts
 for file in import/rcra/load*csv.sh; do
-  bash $file
+  bash $file $RCRA_DIR
 done
