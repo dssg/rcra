504d6b342d3b88b48ff58f96c104f5d9e53bcffe
jzanzig <jzanzig@ip-10-24-1-5.us-west-2.compute.internal>
2015-10-15 18:07:43 +0000

    fix rcra import scripts

--- import/rcra/create_rcra_A_csv.sh
+++ import/rcra/create_rcra_A_csv.sh
@@ -10,7 +10,7 @@ in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/aevent_schema.csv $RCRA_DIR/a2.txt > 
 
 in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/aauthority_schema.csv $RCRA_DIR/a3.txt > $OUTPUT_DIR/aauthority.csv
 
-in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/aln_area_event.csv $RCRA_DIR/a4.txt > $OUTPUT_DIR/aln_area_event.csv
+in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/aln_area_event_schema.csv $RCRA_DIR/a4.txt > $OUTPUT_DIR/aln_area_event.csv
 
 in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/aln_event_authority_schema.csv $RCRA_DIR/a5.txt > $OUTPUT_DIR/aln_event_authority.csv
 
@@ -18,7 +18,7 @@ in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/alu_ca_event_schema.csv $RCRA_DIR/a6.
 
 in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/alu_authority_schema.csv $RCRA_DIR/a7.txt > $OUTPUT_DIR/alu_authority.csv
 
-in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/aln_authority_citation_schema.csv $RCRA_DIR/a8.txt > $OUTPUT_DIR/aln_authority.csv
+in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/aln_authority_citation_schema.csv $RCRA_DIR/a8.txt > $OUTPUT_DIR/aln_authority_citation.csv
 
 in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/alu_statutory_citation_schema.csv $RCRA_DIR/a9.txt > $OUTPUT_DIR/alu_statutory_citation.csv
 
--- import/rcra/create_rcra_F_csv.sh
+++ import/rcra/create_rcra_F_csv.sh
@@ -6,8 +6,7 @@ OUTPUT_DIR=$3
 
 in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/fcost_estimate_schema.csv $RCRA_DIR/fcost_estimate.txt > $OUTPUT_DIR/fcost_estimate.csv
 
-in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/fln_cost_mechanism_detail_schema.csv $RCRA_DIR/fln_cost_mechanism_detail.txt > $OUTPUT_DIR/fln_cost_mechanism.csv
-
+in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/fln_cost_mechanism_detail_schema.csv $RCRA_DIR/fln_cost_mechanism_detail.txt > $OUTPUT_DIR/fln_cost_mechanism_detail.csv
 
 in2csv -e iso-8859-1 -f fixed -s $DICT_DIR/fmechanism_detail_schema.csv $RCRA_DIR/fmechanism_detail.txt > $OUTPUT_DIR/fmechanism_detail.csv
 
