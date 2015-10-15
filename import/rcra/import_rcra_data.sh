7928943924eb9bf5a9f9b4dfd012c8cf5af9dbcc
jzanzig <jzanzig@ip-10-24-1-5.us-west-2.compute.internal>
2015-10-15 17:32:14 +0000

    fix rcra import

--- import/rcra/import_rcra_data.sh
+++ import/rcra/import_rcra_data.sh
@@ -4,7 +4,13 @@ RCRA_DIR=$1
 
 # Make the load scripts and SQL files
 psql -f import/rcra/create_schema_rcra.sql
-psql -f import/rcra/create_table_\*.sql
+psql -f import/rcra/create_table_A.sql
+psql -f import/rcra/create_table_C.sql
+psql -f import/rcra/create_table_F.sql
+psql -f import/rcra/create_table_P.sql
+psql -f import/rcra/create_table_H.sql
+psql -f import/rcra/create_table_BR.sql
+psql -f import/rcra/create_table_gis.sql
 
 # Loop over the load scripts
 for file in import/rcra/load*csv.sh; do
