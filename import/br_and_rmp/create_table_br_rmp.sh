b510f8778d84797ffd4dcca9201c2d1063fe023f
jzanzig <jzanzig@ip-10-24-1-5.us-west-2.compute.internal>
2015-10-19 21:05:22 +0000

    removed schema script

--- import/br_and_rmp/create_table_br_rmp.sh
+++ import/br_and_rmp/create_table_br_rmp.sh
@@ -7,10 +7,10 @@ psql -f import/br_and_rmp/drop_table_br_rmp.sql
 psql -f import/br_and_rmp/create_table_br_rmp.sql
 
 echo loading export_CAA112r_Insp2.csv from ECHO dashboard into the database ...
-cat $BR_RMP_DIR/export_CAA112r_Insp2.csv | psql -c '\COPY "br_and_rmp.caa_inspections" FROM STDIN WITH CSV HEADER;'
+cat $BR_RMP_DIR/export_CAA112r_Insp2.csv | psql -c '\COPY br_and_rmp.caa_inspections FROM STDIN WITH CSV HEADER;'
 echo export_CA112r_Insp2.csv has been loaded into the database as br_and_rmp.caa_inspections.
 
 
 echo loading br_reporting_complete.csv into the database ...
-cat $BR_RMP_DIR/br_reporting_complete.csv | psql -c '\COPY "br_and_rmp.brs_all" FROM STDIN WITH CSV HEADER;'
+cat $BR_RMP_DIR/br_reporting_complete.csv | psql -c '\COPY br_and_rmp.brs_all FROM STDIN WITH CSV HEADER;'
 echo br_reporting_complete.csv has been loaded into the database as brs_all.
