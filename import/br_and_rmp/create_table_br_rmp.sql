d6b1c826592e7a7c38508395066b15fc4c507fdc
jzanzig <jzanzig@ip-10-24-1-5.us-west-2.compute.internal>
2015-11-09 17:36:03 +0000

    fix br import

--- import/br_and_rmp/create_table_br_rmp.sql
+++ import/br_and_rmp/create_table_br_rmp.sql
@@ -61,48 +61,6 @@ CREATE TABLE br_and_rmp.brs_all (
 	management_category VARCHAR
 );
 
---------------------------------
----change report cycle into date 
---------------------------------
-
--- First, add new column with date format
-alter table br_and_rmp.brs_all add column date_year date; 
-
--- convert dates column into date format and put in date_format column
-update br_and_rmp.brs_all
-set date_year = to_date("report_cycle", 'YYYY-MM');
-
-----------------------------------------------
--- first convert waste numbers into floats to later sum
----------------------------------------------- 
-alter table br_and_rmp.brs_all add column gen_tons real;
-alter table br_and_rmp.brs_all alter column gen_tons type float using "generation_tons"::float;
-alter table br_and_rmp.brs_all add column man_tons real;
-alter table br_and_rmp.brs_all alter column man_tons type float using "managed_tons"::float;
-alter table br_and_rmp.brs_all add column ship_tons real;
-alter table br_and_rmp.brs_all alter column ship_tons type float using "shipped_tons"::float;
-alter table br_and_rmp.brs_all add column rec_tons real;
-alter table br_and_rmp.brs_all alter column rec_tons type float using "received_tons"::float;
-
--------------------------------------
---- Waste shipped but not generated or managed 
--------------------------------------
-alter table br_and_rmp.brs_all add column ship_not_man_or_gen int;
-
-update br_and_rmp.brs_all
-	set rec_not_man_or_gen = case when (ship_tons > 0 and man_tons = 0) and
-	(ship_tons > 0 and gen_tons = 0) then 1 else 0 end;
-
-------------------------------------
--- Waste receieved by not generated or managed
-------------------------------------
-alter table br_and_rmp.brs_all add column rec_not_man_or_gen int;	
-
-
-update br_and_rmp.brs_all
-	set rec_not_man_or_gen = case when (rec_tons > 0 and man_tons = 0) and
-	(rec_tons > 0 and gen_tons = 0) then 1 else 0 end;
-		
 DROP TABLE IF EXISTS br_and_rmp.caa_inspections;
 
 CREATE TABLE br_and_rmp.caa_inspections (
