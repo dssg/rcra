7c405a05155d291f561c29326297a949a84bf7e2
jzanzig <jzanzig@ip-10-16-136-233.us-west-2.compute.internal>
2015-12-31 20:09:36 +0000

    fix import

--- import/rcra/xlsx2import.py
+++ import/rcra/xlsx2import.py
@@ -3,22 +3,21 @@ import re, unicodedata, os, sys
 
 data_dictionary = sys.argv[1]
 output_dir = sys.argv[2]
+create_table = sys.argv[3]
 
-def spaces_to_snake(column_name):
+def column_to_snake(column_name):
     
     """
-    converts a string that has spaces into snake_case
-    Example:
-        print camel_to_snake("KENNY BROUGHT HIS WIFE")
-        > KENNY_BROUGHT_HIS_WIFE
-    To see how to apply this to camel case, see:
-        http://stackoverflow.com/questions/1175208/elegant-python-function-to-convert-camelcase-to-camel-case
+    converts a string that has spaces into snake_case, truncates names, and removes digits
+    to make strings acceptable PostgreSQL column names
     """
 
     s = re.sub(r"\s+", '_', column_name)
     s = re.sub(r'/|-', '_', s)
     s = re.sub(r'\(|\)|\*','',s)
-    
+    s = re.sub('^\d+', '', s)
+    s = s[:60]
+
     return s.lower()    
 
 xl = pd.ExcelFile(data_dictionary)
@@ -26,7 +25,7 @@ names = xl.sheet_names
 
 os.chdir(output_dir)
 
-with open("create_table_rcra.sql", 'a') as f:
+with open(create_table, 'a') as f:
 
     for name in names:
     
@@ -37,7 +36,7 @@ with open("create_table_rcra.sql", 'a') as f:
 
         df.columns = ['no.','start','column','type','length']
         df['column'] = df['column'].map(lambda x: unicodedata.normalize('NFKD', x).encode('ascii','ignore') if isinstance(x, unicode) else x)
-        df['column'] = df['column'].map(lambda x: spaces_to_snake(x))
+        df['column'] = df['column'].map(lambda x: column_to_snake(x))
 
         schema = df.ix[:, ['column', 'start', 'length']]
         schema.to_csv(table + '_schema.csv', index = False, encoding = 'utf-8')
