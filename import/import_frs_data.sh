0f6d08958839c2c5923eb0456928e1aea06098fe
jzanzig <jmlzanzig@gmail.com>
2016-01-04 11:06:50 -0600

    update import_frs_data.sh

--- import/import_frs_data.sh
+++ import/import_frs_data.sh
@@ -2,6 +2,8 @@
 
 FRS_DIR=$1
 
-cat $FRS_DIR/FRS_FACILITIES.csv | psql -c '\COPY frs.facilities FROM STDIN WITH CSV HEADER;'
-cat $FRS_DIR/FRS_PROGRAM_LINKS.csv | psql -c '\COPY frs.program_links FROM STDIN WITH CSV HEADER;'
+psql -v ON_ERROR_STOP=1 -f import/create_table_frs.sql
+
+cat $FRS_DIR/FRS_FACILITIES.csv | psql -v ON_ERROR_STOP=1 -c '\COPY frs.facilities FROM STDIN WITH CSV HEADER;'
+cat $FRS_DIR/FRS_PROGRAM_LINKS.csv | psql -v ON_ERROR_STOP=1 -c '\COPY frs.program_links FROM STDIN WITH CSV HEADER;'
 
