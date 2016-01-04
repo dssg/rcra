996b2c7b1913b6ad45e9e46fb023906462143d65
jzanzig <jmlzanzig@gmail.com>
2016-01-04 10:39:38 -0600

    update import frs

--- import/import_frs_data.sh
+++ import/import_frs_data.sh
@@ -1,8 +1,6 @@
-#!/bin/bash -xv
+#!/bin/bash 
 
 FRS_DIR=$1
-
-psql -f import/create_table_frs.sql
 
 cat $FRS_DIR/FRS_FACILITIES.csv | psql -c '\COPY frs.facilities FROM STDIN WITH CSV HEADER;'
 cat $FRS_DIR/FRS_PROGRAM_LINKS.csv | psql -c '\COPY frs.program_links FROM STDIN WITH CSV HEADER;'
