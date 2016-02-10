9e5b69b19444d71f947024eb7e11f1c858e6cbc0
potash <eric@k2co3.net>
2016-02-10 20:11:55 +0000

    fix drakefiles

--- import/create_indexes.sql
+++ import/create_indexes.sql
@@ -1,2 +1,5 @@
 drop index if exists cmecomp3_handler_id_evaluation_start_date_idx;
 create index cmecomp3_handler_id_evaluation_start_date_idx on rcra.cmecomp3 (handler_id, evaluation_start_date);
+
+drop index if exists hhandler_handler_id_idx;
+create index hhandler_handler_id_idx on rcra.hhandler (epa_handler_id);
