a7198420baad0ccb38b97c0f2111f3f1972f5661
Jimmy Jin <yichi.jin@gmail.com>
2016-07-01 14:59:05 +0000

    Initial fixes to create_tri_csv.sh

--- import/tri/create_tri_csv.sh
+++ import/tri/create_tri_csv.sh
@@ -16,15 +16,19 @@ mv $TRI_DIR/tri_${i}.txt $TRI_DIR/tri_${i}.tmp
 cut -f 1-$[$ncols-2] <$TRI_DIR/tri_${i}.tmp >$TRI_DIR/tri_${i}.txt &&
 rm $TRI_DIR/tri_${i}.tmp  
 
-echo "converting to csv"
 # convert to csv
-in2csv -t -f csv -e iso-8859-1 $TRI_DIR/tri_${i}.txt > $TRI_DIR/tri_${i}.csv
+#in2csv -t -f csv -e iso-8859-1 $TRI_DIR/tri_${i}.txt > $TRI_DIR/tri_${i}.csv
 #sed -s 's/\t/,/g' $TRI_DIR/tri_${i}.txt > $TRI_DIR/tri_${i}.csv
 
-echo "importing"
+echo "creating table"
 # copy into database
 psql -v ON_ERROR_STOP=1 -f import/tri/create_table_${i}.sql  
-tr -d '(\r|\000)' < $TRI_DIR/tri_${i}.csv | psql -v ON_ERROR_STOP=1 -c '\'"COPY tri.type_${i} FROM STDIN WITH CSV HEADER;"
 
+if [${i} -eq 1] 
+    then sed -i '1661525s/BOYLE                /BOYLE  /;1661525s/COM          $/COM                   /g' $TRI_DIR/tri_${i}.txt
+fi
+
+echo "importing"
+cat $TRI_DIR/tri_${i}.txt | uconv -f iso-8859-1 -t utf-8 | awk 'BEGIN {FS=IFS=OFS="\t"} { for (N=1; N<NF;N++) sub(/NA/, "", $N)} 1' | psql -v ON_ERROR_STOP=1 -c "\COPY tri.type_${i} FROM STDIN WITH CSV HEADER QUOTE E'\b' DELIMITER AS E'\t'"
 
 
