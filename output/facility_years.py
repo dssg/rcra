48c910492c59dc37df0832d5f54707663a7bad1b
Eric Potash <eric@k2co3.net>
2016-07-08 17:00:37 +0000

    python3 print

--- output/facility_years.py
+++ output/facility_years.py
@@ -4,4 +4,4 @@ with open(sys.argv[1], 'r') as sql_file:
     sql = sql_file.read().format(min_year=sys.argv[2], max_year=sys.argv[3],
             month=sys.argv[4], day=sys.argv[5])
 
-    print sql
+    print(sql)
