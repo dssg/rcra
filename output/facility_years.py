de3502630159cc98150e53f25c443c4efceaaa64
Eric Potash <eric@k2co3.net>
2016-07-08 16:46:19 +0000

    remove unnecessary drain import

--- output/facility_years.py
+++ output/facility_years.py
@@ -1,10 +1,7 @@
 import sys
-from drain import util
 
 with open(sys.argv[1], 'r') as sql_file:
     sql = sql_file.read().format(min_year=sys.argv[2], max_year=sys.argv[3],
             month=sys.argv[4], day=sys.argv[5])
 
     print sql
-#engine = util.create_engine()
-#util.execute_sql(sql, engine)
