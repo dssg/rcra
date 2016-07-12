c510d319c7950b66483de1a53a0263e5620c6249
Eric Potash <eric@k2co3.net>
2016-07-12 16:29:40 +0000

    drop indexes before creation

--- import/create_indexes.sql
+++ import/create_indexes.sql
@@ -1,3 +1,5 @@
-create index on rcra.cmecomp3 (handler_id, evaluation_start_date);
+drop index if exists rcra.cmecomp3_handler_id_evaluation_start_date_idx;
+create index cmecomp3_handler_id_evaluation_start_date_idx on rcra.cmecomp3 (handler_id, evaluation_start_date);
 
-create index on rcra.hhandler (epa_handler_id);
+drop index if exists rcra.hhandler_epa_handler_id_idx;
+create index hhandler_epa_handler_id_idx on rcra.hhandler (epa_handler_id);
