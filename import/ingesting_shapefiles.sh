55e18bf0c258e9923d587df125430c59f346a09c
OpsWorks user mkamenets <mkamenets@nysdec.localdomain>
2016-08-05 16:45:32 +0000

    adding changes to ingesting_shapefiles.sh

--- import/ingesting_shapefiles.sh
+++ import/ingesting_shapefiles.sh
@@ -6,6 +6,9 @@
 curl -O http://www2.census.gov/geo/tiger/TIGER2007FE/fe_2007_us_state.zip
 curl -O http://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_zcta510_500k.zip
 
+#Include in Flood Hazard Data (had to download separately and upload; couldn't curl
+
+
 #Load zipcode data into postgress
 # SRID_old: 4269 (US Census projection)
 # SRID_new: 4326 (Google projection)
@@ -13,3 +16,4 @@ curl -O http://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_zcta510_500k.zi
 
 shp2pgsql -I -s 4269:4326 -d cb_2015_us_zcta510_500k geo.zipcode | psql 
 shp2pgsql -I -s 4269:4326 -d fe_2007_us_state geo.states | psql 
+shp2pgsql -I -s 4269:4326 -d NFHL_36_20160712.zip | psql
