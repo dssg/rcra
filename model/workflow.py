d95330272566ce82ae86465e0ae0cc58c8c8ad2f
OpsWorks user dmagee <dmagee@nysdec.localdomain>
2016-08-08 19:56:21 +0000

    fixed new pipeline problem

--- model/workflow.py
+++ model/workflow.py
@@ -86,16 +86,16 @@ svm_search = [{'__class_name__':['sklearn.svm.LinearSVC'],
 
 ### Newly created workflows for NYSDEC project
 def violation_state_original_data():
-    return models(transform_search= dict(train_years=range(2,4), year=[2013,2015],exclude =[['manifest_.*','br_.*']], **violation_state_args), estimator_search=forest)
+    return models(transform_search= dict(train_years=range(2,6), year=range(2012,2016),exclude =[['manifest_.*','br_.*']], **violation_state_args), estimator_search=forest)
 
 def violation_state_manifest_added():
-    return models(transform_search= dict(train_years=range(2,4), year=[2013,2015],exclude=[['br_.*']], **violation_state_args), estimator_search=forest)
+    return models(transform_search= dict(train_years=range(2,6), year=range(2012,2016),exclude=[['br_.*']], **violation_state_args), estimator_search=forest)
 
 def violation_state_br_added():
-    return models(transform_search= dict(train_years=range(2,4), year=[2013,2015], exclude=[['manifest_.*']], **violation_state_args), estimator_search=forest)
+    return models(transform_search= dict(train_years=range(2,6), year=range(2012,2016), exclude=[['manifest_.*']], **violation_state_args), estimator_search=forest)
 
 def violation_state_manifest_br_added():
-    return models(transform_search= dict(train_years=range(2,4), year=[2013,2015], **violation_state_args), estimator_search=forest)
+    return models(transform_search= dict(train_years=range(2,6), year=range(2013,2016), **violation_state_args), estimator_search=forest)
 
 
 
--- output/manifest.py
+++ output/manifest.py
@@ -1,10 +1,15 @@
 import logging
 from datetime import date
+import re
 
 from drain.util import day
 from drain.data import FromSQL
 from drain.aggregate import Aggregate, Count, days
 from drain.aggregation import SpacetimeAggregation
+
+WASTE_CODE_PREFIXES = ('P', 'U', 'D', 'F')
+# acute waste is defined as either P waste or one of these six F wastes
+ACUTE_WASTE_REGEX = re.compile('^(P|F02[012367]$)')
 
 class ManifestAggregation(SpacetimeAggregation):
     def __init__(self, spacedeltas, dates, **kwargs):
@@ -14,21 +19,30 @@ class ManifestAggregation(SpacetimeAggregation):
         if not self.parallel:
             self.manifest = FromSQL(
                 query="""select gen_rcra_id as rcra_id, *,
-                ARRAY_REMOVE(ARRAY[waste_code_1, waste_code_2, waste_code_3,
-                    waste_code_4, waste_code_5, waste_code_6], NULL) as waste_codes
-                from manifest.new_york where substring(gen_rcra_id for 2) = 'NY' """, 
+                ARRAY_REMOVE(ARRAY_REMOVE(
+                    ARRAY[waste_code_1, waste_code_2, waste_code_3,
+                          waste_code_4, waste_code_5, waste_code_6], 
+                    NULL),'') as waste_codes 
+                from manifest.new_york where substring(gen_rcra_id for 2) = 'NY' """,
+                # Puts waste codes into array and removes NULLS and empty strings 
                 tables=['manifest.new_york'], parse_dates=['gen_sign_date'], target=True)
             self.inputs = [self.manifest]
 
     def get_aggregates(self, date, delta):
+        
         aggregates = [
             Count(name='line_items'),
             #Count(booleans, prop=True),
             Aggregate('approx_qty', ['max','min','mean','std','skew'], name='approx_qty'),
-            Aggregate(lambda m: m.waste_codes.apply(lambda w: sum(code[0] == 'P' for code in w)>0) ,['any'], name = 'waste_code_p'),
-            Aggregate(lambda m: m.waste_codes.apply(lambda w: sum(code[0] == 'U' for code in w)>0) ,['any'], name = 'waste_code_u'),
-            Aggregate(lambda m: m.waste_codes.apply(lambda w: sum(code[0] == 'D' for code in w)>0) ,['any'], name = 'waste_code_d'),
-            Aggregate(lambda m: m.waste_codes.apply(lambda w: sum(code[0] == 'F' for code in w)>0) ,['any'], name = 'waste_code_f'),
-            Aggregate(lambda m: m.waste_codes.apply(lambda w: sum(code[0] == 'P' or code == 'F020' or code == 'F021' or code == 'F022' or code == 'F023' or code == 'F026' or code == 'F027' for code in w)>0) ,['any'], name = 'waste_acute') ]
+            Aggregate([has_waste_type(p) for p in WASTE_CODE_PREFIXES],
+                ['any'], name = ['waste_code_%s' % p for p in WASTE_CODE_PREFIXES]),
+            Aggregate(lambda m: m.waste_codes.apply(lambda w: sum(is_acute_waste(code) for code in w)>0),
+                ['any'], name = 'waste_acute') ]
 
         return aggregates
+
+def has_waste_type(prefix):
+    return lambda m: m.waste_codes.apply(lambda w: sum(code[0] == prefix for code in w)>0)
+
+def is_acute_waste(code):
+    return ACUTE_WASTE_REGEX.match(code) is not None
