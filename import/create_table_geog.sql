830eedc9d595eeaddadb2a663e09fdedc6fe2833
OpsWorks user mkamenets <mkamenets@nysdec.localdomain>
2016-08-12 16:05:34 +0000

    added different geometry view to flood haz/ny zip codes data

--- import/create_table_geog.sql
+++ import/create_table_geog.sql
@@ -6,9 +6,12 @@ Date: 4/5 August 2016
 
 
 --Create view that is the intersection of New York state and its zip codes; we include those zip codes that are touching the state boundary
+
+--Create the geography schema
 CREATE SCHEMA IF NOT EXISTS geo;
 
 
+--View to load the zip codes in NY only
 DROP VIEW IF EXISTS geo.NY_zipcodes;
 
 CREATE VIEW geo.NY_zipcodes AS (
@@ -18,8 +21,24 @@ CREATE VIEW geo.NY_zipcodes AS (
     WHERE name = 'New York');
 
 
-DROP VIEW geo.floodhaz;
 
-CREATE VIEW geo.floodhaz AS (
-	SELECT 
+DROP VIEW geo.NY_zipcodes;
+
+CREATE VIEW geo.NY_zipcodes AS
+    select * from
+    geo.zipcode as zip
+    where ST_TOUCHES(zip.geom,
+	                     (
+				    SELECT geom from geo.ny_zipcode
+				    where name = 'New York')
+			    );	
+
+--view to get the flood haz in NY zip codes only
+drop view geo.flood_ny_ar;
+create view geo.flood_ny_ar as
+	select distinct nyz.geom, nyz.gid from geo.floodhaz_ar fld inner join
+			geo.ny_zipcodes nyz on ST_Intersects(fld.geom, nyz.geom);
+
+
+
 
