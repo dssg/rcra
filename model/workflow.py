c25ee9a650ac46c834cbeb77e3f24e2b9c8a01b6
Eric Potash <eric@k2co3.net>
2017-02-17 19:07:29 +0000

    adaboost and gradient

--- model/workflow.py
+++ model/workflow.py
@@ -8,10 +8,11 @@ YEARS = range(2010, 2015+1)
 
 ACTIVE = 'handler_received and (active_today or (handler_age < 365) or br)'
 LQG = '(manifest_monthly_3y_approx_qty_max >= 2200)'
-LQG_UNINSPECTED = '~(last_investigation_days < 1825) and (manifest_monthly_3y_approx_qty_max >= 2200)'
+LQG_UNINSPECTED_3Y = '~(last_investigation_days < 1095) and (manifest_monthly_3y_approx_qty_max >= 2200)'
+LQG_UNINSPECTED_5Y = '~(last_investigation_days < 1825) and (manifest_monthly_3y_approx_qty_max >= 2200)'
 R2_LQG_UNINSPECTED = "~(last_investigation_days < 1825) and state_district == 'NYSDEC R2' and (manifest_monthly_3y_approx_qty_max >= 2200)"
 
-QUERIES = [ACTIVE, LQG, LQG_UNINSPECTED]
+QUERIES = [ACTIVE, LQG, LQG_UNINSPECTED_3Y]
 
 aggregations = {a: {level: indexdelta[1]  for level, indexdelta in s.items()}
                 for a,s in spacedeltas.iteritems()}
@@ -37,29 +38,6 @@ violation_args = dict(
     train_query='evaluation', 
     evaluation=False
 )
-
-region_4_violation_types = [
-        '262.A',
-        # 264.*
-        '264.A', '264.AA', '264.B', '264.BB', '264.C', '264.CC', '264.D', '264.DD', '264.E', 
-        '264.EE', '264.F', '264.G', '264.H', '264.I', '264.J', '264.K', '264.L', '264.M', 
-        '264.N', '264.O', '264.S', '264.W', '264.X', 
-        # 268.*
-        '268.A', '268.B', '268.C', '268.D', '268.E', 
-        # 270.*
-        '270.A', '270.B', '270.C', '270.D', '270.F', '270.G', '270.H', '270.I', 
-        '265.J', '265.BB', '265.CC'
-]
-
-# use where() so that it's null when not evaluated by epa
-region_4_outcome_expr = "aux.violation_types_epa.apply(lambda v, vt=set([%s]): not vt.isdisjoint(v)).where(aux.evaluation_epa)" % \
-        str.join(',', ["'%s'" % v for v in region_4_violation_types])
-
-region_4_args = dict(
-    outcome_expr=region_4_outcome_expr,
-    train_query='aux.evaluation_epa',
-    region=4,
-    evaluation=False)
 
 evaluation_state_args = dict(
     outcome_expr='aux.evaluation_state', 
@@ -93,14 +71,14 @@ adaboost = {'_class_name':['sklearn.ensemble.AdaBoostClassifier'],
             'n_estimators':[25],
             'learning_rate':[1]}
 
-gradient= {'_class_name':['sklearn.ensemble.GradientBoostingClassifier'],
+gradient = {'_class_name':['sklearn.ensemble.GradientBoostingClassifier'],
             'loss':['deviance'],
             'learning_rate':[0.1],
             'n_estimators':[100],
             'max_depth':[3]}
 
 svm = {'_class_name':['sklearn.svm.LinearSVC'],
-        'C':[.01], 'penalty':['l2'], 'dual':[False]}
+        'C':[.01], 'penalty':['l1'], 'dual':[False]}
 
 forest_search = {'_class_name':['sklearn.ensemble.RandomForestClassifier'], 
         'n_estimators':[500],
@@ -112,12 +90,19 @@ logit_search = {'_class_name':['sklearn.linear_model.LogisticRegression'],
         'penalty':['l1','l2'], 'C':[.01,.1,1,10]}
 
 adaboost_search = {'_class_name':['sklearn.ensemble.AdaBoostClassifier'],
-            'n_estimators':[20,30,50]}
+            'n_estimators':[25,50,100]}
 
 svm_search = [{'_class_name':['sklearn.svm.LinearSVC'],
         'C':[.01,.1,1], 'penalty':['l2'], 'dual':[True,False]},
         {'_class_name':['sklearn.svm.LinearSVC'],
         'C':[.01,.1,1], 'penalty':['l1'], 'dual':[False]}]
+
+gradient_search = {'_class_name':['sklearn.ensemble.GradientBoostingClassifier'],
+            'loss':['deviance'],
+            'learning_rate':[.01, 0.1, 1],
+            'n_estimators':[50, 100, 200],
+            'max_depth':[2,3,4]}
+
 
 ### the "baseline" models are random forests with manually selected train_years
 def violation_state_baseline():
@@ -245,7 +230,8 @@ def evaluation_state_aggregation_levels():
 
 # vary train years
 def evaluation_state_train_years():
-    return models(transform_search= dict(train_years=range(1,8), year=YEARS, **evaluation_state_args), estimator_search=logit)
+    return models(transform_search=dict(train_years=range(1,8), year=YEARS, **evaluation_state_args), 
+                  estimator_search=logit)
 
 def evaluation_state_train_queries():
     transform_search = util.dict_merge(evaluation_state_args, 
@@ -257,10 +243,15 @@ def evaluation_state_train_queries():
 
 # grid searches for various model classes
 def violation_state_adaboost():
-    return models(transform_search= dict(train_years=range(2,5), year=range(2012,2016), **violation_state_args), estimator_search=adaboost_search)
+    return models(transform_search=dict(train_years=[5], year=YEARS, **violation_state_args), estimator_search=adaboost_search)
+
+def violation_state_gradient():
+    return models(transform_search=dict(train_years=[5], year=YEARS, **violation_state_args), estimator_search=gradient_search)
+
 
 def violation_state_forest():
-    return models(transform_search= dict(train_years=range(2,5), year=range(2012,2016), **violation_state_args), estimator_search=forest_search)
+    return models(transform_search=dict(train_years=[5], year=YEARS, **violation_state_args),
+                  estimator_search=forest_search)
 
 def violation_state_logit():
     transform_search=dict(train_years=[5], year=YEARS, **violation_state_args)
@@ -307,6 +298,29 @@ def violation_fast():
 
 def violation():
     return models(transform_search= dict(train_years=2, **violation_args), estimator_search=forest)
+
+region_4_violation_types = [
+        '262.A',
+        # 264.*
+        '264.A', '264.AA', '264.B', '264.BB', '264.C', '264.CC', '264.D', '264.DD', '264.E', 
+        '264.EE', '264.F', '264.G', '264.H', '264.I', '264.J', '264.K', '264.L', '264.M', 
+        '264.N', '264.O', '264.S', '264.W', '264.X', 
+        # 268.*
+        '268.A', '268.B', '268.C', '268.D', '268.E', 
+        # 270.*
+        '270.A', '270.B', '270.C', '270.D', '270.F', '270.G', '270.H', '270.I', 
+        '265.J', '265.BB', '265.CC'
+]
+
+# use where() so that it's null when not evaluated by epa
+region_4_outcome_expr = "aux.violation_types_epa.apply(lambda v, vt=set([%s]): not vt.isdisjoint(v)).where(aux.evaluation_epa)" % \
+        str.join(',', ["'%s'" % v for v in region_4_violation_types])
+
+region_4_args = dict(
+    outcome_expr=region_4_outcome_expr,
+    train_query='aux.evaluation_epa',
+    region=4,
+    evaluation=False)
 
 def violation_region_4():
     return models(transform_search= dict(train_years=5, **region_4_args), estimator_search=forest)
