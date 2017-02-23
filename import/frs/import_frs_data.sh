69c2514ca693b8dc1733ea553caa786a43dc8762
Eric Potash <eric@k2co3.net>
2017-02-23 17:28:38 +0000

    exit on error

--- import/frs/import_frs_data.sh
+++ import/frs/import_frs_data.sh
@@ -4,6 +4,6 @@ FRS_DIR=$1
 
 psql -v ON_ERROR_STOP=1 -f import/frs/create_table_frs.sql
 
-cat $FRS_DIR/FRS_FACILITIES.csv | psql -v ON_ERROR_STOP=1 -c '\COPY frs.facilities FROM STDIN WITH CSV HEADER;'
+cat $FRS_DIR/FRS_FACILITIES.csv | psql -v ON_ERROR_STOP=1 -c '\COPY frs.facilities FROM STDIN WITH CSV HEADER;' || exit 1
 cat $FRS_DIR/FRS_PROGRAM_LINKS.csv | psql -v ON_ERROR_STOP=1 -c '\COPY frs.program_links FROM STDIN WITH CSV HEADER;'
 
