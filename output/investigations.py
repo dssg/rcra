279060fac40fbb6050185bde41c25ade04248fd4
Eric Potash <eric@k2co3.net>
2017-02-24 05:19:01 +0000

    investigations features

--- output/investigations.py
+++ output/investigations.py
@@ -5,7 +5,7 @@ import numpy as np
 import itertools
 
 from drain import util, data, aggregate
-from drain.data import date_censor_sql, ToHDF, FromSQL, Revise, Merge
+from drain.data import date_censor_sql, ToHDF, FromSQL, Revise, Merge, BinarizeSets
 from drain.step import Step
 from drain.aggregation import SpacetimeAggregation, SimpleAggregation
 from drain.aggregate import Aggregate, Count, aggregate_counts, days
@@ -20,7 +20,7 @@ from . import investigations_sql, facilities
 
 date_columns = investigations_sql.date_columns
 parse_dates = ['min_' + c for c in date_columns] + \
-               ['max_' + c for c in date_columns] + ['start_date']
+              ['max_' + c for c in date_columns] + ['start_date']
 
 outcomes = ['violation', 'violation_epa', 'violation_state', 
             'formal_enforcement', 'enforcement_state', 
@@ -31,7 +31,10 @@ amounts = ['final_monetary_amount', 'paid_amount', 'proposed_penalty_amount',
             'expenditure_amount', 'final_count', 'final_amount']
 
 flags = ['corrective_action_component', 'citizen_complaint', 
-        'multimedia_inspection', 'sampling', 'not_subtitle_c']
+         'multimedia_inspection', 'sampling', 'not_subtitle_c']
+
+EVALUATION_TYPES = ['CEI', 'FCI', 'NRR', 'CSE', 'SNY', 'FRR', 'FUI', 'SNN', 
+                    'CAV', 'CDI', 'GME', 'FSD', 'OAM', 'CAC', 'NIR']
 
 class InvestigationsAggregation(SpacetimeAggregation):
     def __init__(self, spacedeltas, dates, parallel=True):
@@ -50,6 +53,8 @@ class InvestigationsAggregation(SpacetimeAggregation):
                     date = self.dates[0],
                     from_sql_args={'parse_dates':parse_dates})
             for j in i.inputs: j.target = True
+            i = BinarizeSets(inputs=[i], cast=True,
+                             columns={'evaluation_types':EVALUATION_TYPES})
             self.inputs = [Merge(inputs=[i, facilities], on='rcra_id')]
 
     def get_aggregates(self, date, delta):
@@ -66,7 +71,8 @@ class InvestigationsAggregation(SpacetimeAggregation):
             Aggregate(days('start_date', date), 'max', name='start_date_days'),
             Aggregate([days('max_' + c, date) for c in date_columns], 'max', 
                 name=[c + '_days' for c in date_columns]),
-
+            Count([lambda i,c=c: i['min_' + c].notnull() for c in date_columns], 
+                  name=date_columns, prop=True),
             Aggregate(days('min_violation_determined_date', 'start_date'), ['min', 'mean', 'max'], 
                     'evaluation_to_violation'),
             Aggregate(days('min_violation_determined_date', 'max_actual_return_to_compliance_date'),
@@ -82,6 +88,7 @@ class InvestigationsAggregation(SpacetimeAggregation):
                     ['max', 'min'], 'sep_completion_overtime'),
             Aggregate(days('min_sep_defaulted_date', 'max_sep_scheduled_completion_date'),
                     ['max', 'min'], 'sep_default_to_completion'),
+            Count(['evaluation_types_' + t for t in EVALUATION_TYPES], prop=True)
             #Aggregate(list_columns, aggregate_counts, fname=False)
         ]
 
